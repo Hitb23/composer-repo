651194499bcfa7f6bf7c1483c602a34738f0d2ef
Hitb23 <hit.bambhroliya@etatvasoft.com>
2025-02-26 09:41:24 +0530

    added sd url

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = FastAPI()
 def call_provider():
     try:
         # Calling the provider container using its service name.
-        response = requests.get("http://provider:8000/dynamodb", timeout=5)
+        response = requests.get("http://provider-service.myservices.local:8000/dynamodb", timeout=5)
         response.raise_for_status()
         return {"provider_response": response.json()}
     except requests.RequestException as e:
